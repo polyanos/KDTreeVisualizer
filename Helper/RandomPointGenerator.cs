4f92bbde1da00a0d701d233f7e38852afd53d49a
Polyanos <gregorhoogstad@gmail.com>
2016-12-15 18:24:48 +0100

    Created a random Point generator with a generalized Interface.

---
+++ Helper/RandomPointGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDTreeVisualizer.Helper
+{
+    class RandomPointGenerator : RandomGenerator<Point>
+    {
+        private Random rng;
+        private int xMax;
+        private int yMax;
+
+        public RandomPointGenerator(int xMax, int yMax, int seed = 0)
+        {
+            rng = seed == 0 ? new Random() : new Random(seed);
+            this.xMax = xMax;
+            this.yMax = yMax;
+        }
+
+
+        public Point[] GenerateMultipleValues(int amount)
+        {
+            Point[] points = new Point[amount];
+
+            for (int x = 0; x < amount; x++)
+            {
+                points[x] = new Point(rng.Next(xMax), rng.Next(yMax));
+            }
+
+            return points;
+        }
+
+        public Point GenerateSingleValue()
+        {
+            return new Point(rng.Next(xMax), rng.Next(yMax));
+        }
+
+    }
+}
